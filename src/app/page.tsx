b4f115a0373535b9061fe29d22e2a6102e3183e9
amansagar0403 <as624@snu.edu.in>
2024-02-19 21:50:44 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,13 @@ import Spline from "@splinetool/react-spline";
 
 export default function Page() {
   return (
-    <div className="flex h-screen">
-      <div className="flex flex-col items-center justify-center w-1/2 p-4">
+    <div className="flex flex-col h-screen">
+      <div className="w-full h-full">
+        <div className="w-full h-full">
+          <Spline scene="https://prod.spline.design/yRqFaY2i1AkuTRS0/scene.splinecode" />
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center w-full p-4">
         <h1 className="text-4xl font-bold font-serif mb-4">Aviral Chawla</h1>
         <div className="flex flex-col items-center mb-4">
           <Link href="/chat">
@@ -15,11 +20,6 @@ export default function Page() {
           <Link href="mailto:ac724@snu.edu.in">
             <button className="w-36 h-10 btn">Mail</button>
           </Link>
-        </div>
-      </div>
-      <div className="w-1/2 h-full">
-        <div className="w-full h-full">
-          <Spline scene="https://prod.spline.design/yRqFaY2i1AkuTRS0/scene.splinecode" />
         </div>
       </div>
     </div>
